1df58d8826e1a3d2d9e32be93b21d56eda11524a
Jahid1604091 <jh409780@gmail.com>
2022-04-22 10:22:45 +0600

    users table migration

---
+++ database/migrations/2022_04_22_101155_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',128);
+            $table->string('email',128)->unique();
+            $table->string('phone_number',32)->unique();
+            $table->string('password',128)->unique();
+            $table->bigInteger('reward_points')->default(0);
+            $table->tinyInteger('email_verified_at')->default(0);
+            $table->tinyInteger('email_verified_token',128)->nullable();
+            $table->string('facebook_id',32)->nullable();
+            $table->string('google_id',32)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
